4d963e0beff6e76578cb170bd286948850beca9e
spimort <spimort@gmail.com>
2025-08-14 07:00:06 -0400

    First iteration for brush numeric selector

---
+++ src/editor_nodes/brush_numeric_selector.cpp
@@ -0,0 +1,146 @@
+#include "brush_numeric_selector.h"
+#include "../misc/setting_contants.h"
+
+#include <godot_cpp/classes/resource_loader.hpp>
+#include <godot_cpp/classes/project_settings.hpp>
+#include <godot_cpp/classes/style_box_flat.hpp>
+#include <godot_cpp/classes/input_event.hpp>
+#include <godot_cpp/classes/input_event_mouse_button.hpp>
+
+using namespace godot;
+
+void BrushNumericSelector::_bind_methods() {}
+
+BrushNumericSelector::BrushNumericSelector() {
+    _brushSizeFactor = 2;
+    _widgetColor = Color::named("WHITE");
+    _minValue = -1;
+    _maxValue = 1;
+
+    _minValue = 0;
+    _maxValue = 200;
+}
+
+BrushNumericSelector::~BrushNumericSelector() {}
+
+void BrushNumericSelector::_ready() {
+    _background = memnew(TextureRect);
+    _background->set_expand_mode(TextureRect::ExpandMode::EXPAND_IGNORE_SIZE);
+    _background->set_texture(ResourceLoader::get_singleton()->load("res://addons/terrabrush/Assets/white_circle.png"));
+    _background->set_modulate(Color::html("#3a3a3a4b"));
+    add_child(_background);
+
+    _brushPreview = memnew(TextureRect);
+    _brushPreview->set_expand_mode(TextureRect::ExpandMode::EXPAND_IGNORE_SIZE);
+    _brushPreview->set_texture(ResourceLoader::get_singleton()->load("res://addons/terrabrush/Assets/brush_preview.png"));
+    _brushPreview->set_anchors_and_offsets_preset(Control::LayoutPreset::PRESET_CENTER);
+    _brushPreview->set_modulate(_widgetColor);
+    add_child(_brushPreview);
+
+    _valueLabel = memnew(Label);
+    _valueLabel->set_horizontal_alignment(HorizontalAlignment::HORIZONTAL_ALIGNMENT_CENTER);
+    _valueLabel->set_vertical_alignment(VerticalAlignment::VERTICAL_ALIGNMENT_CENTER);
+    _valueLabel->set_anchor(Side::SIDE_LEFT, 0.5);
+    _valueLabel->set_anchor(Side::SIDE_RIGHT, 0.5);
+    _valueLabel->set_anchor(Side::SIDE_TOP, 0.5);
+    _valueLabel->set_anchor(Side::SIDE_BOTTOM, 0.5);
+    _valueLabel->set_offset(Side::SIDE_LEFT, -11.5);
+    _valueLabel->set_offset(Side::SIDE_RIGHT, -14.0);
+    _valueLabel->set_offset(Side::SIDE_TOP, 11.5);
+    _valueLabel->set_offset(Side::SIDE_BOTTOM, 14.0);
+    _valueLabel->set_h_grow_direction(GrowDirection::GROW_DIRECTION_BOTH);
+    _valueLabel->set_v_grow_direction(GrowDirection::GROW_DIRECTION_BOTH);
+    _valueLabel->set((StringName)"theme_override_colors/font_color", Color::named("WHITE"));
+    _valueLabel->set((StringName)"theme_override_colors/font_outline_color", Color::html("#00151f"));
+    _valueLabel->set((StringName)"theme_override_constants/outline_size", 3);
+    _valueLabel->set((StringName)"theme_override_font_sizes/font_size", 20);
+
+    Ref<StyleBoxFlat> valueLabelNormalStyle = memnew(StyleBoxFlat);
+    valueLabelNormalStyle->set_bg_color(Color::html("#00151f"));
+    valueLabelNormalStyle->set_corner_radius_all(5);
+    valueLabelNormalStyle->set_expand_margin_all(5);
+
+    _valueLabel->set((StringName)"theme_override_styles/normal", valueLabelNormalStyle);
+    add_child(_valueLabel);
+
+    Color iconsColor = ProjectSettings::get_singleton()->get_setting(SettingContants::get_iconsColor());
+    _valueLabel->set((StringName)"theme_override_colors/font_outline_color", iconsColor);
+    Ref<StyleBoxFlat> normalStyle = _valueLabel->get((StringName)"theme_override_styles/normal");
+    normalStyle->set_bg_color(iconsColor);
+}
+
+void BrushNumericSelector::_process(double delta) {
+    updateValue(getMouseDistance());
+}
+
+void BrushNumericSelector::_gui_input(const Ref<InputEvent> &event) {
+    Ref<InputEventMouseButton> inputButton = Object::cast_to<InputEventMouseButton>(*event);
+
+    if (!inputButton.is_null()) {
+        if (inputButton->get_button_index() == MouseButton::MOUSE_BUTTON_LEFT) {
+            auto distance = getMouseDistance();
+            if (!_onValueSelected.is_null()) {
+                _onValueSelected.call(distance);
+            }
+        } else if (!_onCancel.is_null()) {
+            _onCancel.call();
+        }
+    }
+}
+
+void BrushNumericSelector::set_brushSizeFactor(const int value) {
+    _brushSizeFactor = value;
+}
+void BrushNumericSelector::set_widgetColor(const Color value) {
+    _widgetColor = value;
+}
+void BrushNumericSelector::set_minValue(const int value) {
+    _minValue = value;
+}
+void BrushNumericSelector::set_maxValue(const int value) {
+    _maxValue = value;
+}
+
+void BrushNumericSelector::set_onValueSelected(const Callable value) {
+    _onValueSelected = value;
+}
+void BrushNumericSelector::set_onCancel(const Callable value) {
+    _onCancel = value;
+}
+
+int BrushNumericSelector::getMouseDistance() {
+    float distance = get_position().distance_to(get_global_mouse_position());
+
+    if (_minValue >= 0) {
+        distance = Math::max(distance, (float)_minValue);
+    }
+
+    if (_maxValue >= 0) {
+        distance = Math::min(distance, (float)_maxValue);
+    }
+
+    return (int) distance;
+}
+
+void BrushNumericSelector::setInitialValue(float value) {
+    set_position(get_position() - Vector2(value * _brushSizeFactor / 2, 0));
+    updateValue(value);
+}
+
+void BrushNumericSelector::updateValue(float value) {
+    auto size = value * _brushSizeFactor;
+    _background->set_size(Vector2(size + BackgroundMargin, size + BackgroundMargin));
+    _background->set_position(Vector2(-((size / 2) + BackgroundMargin / 2), -((size / 2) + BackgroundMargin / 2)));
+
+    _brushPreview->set_size(Vector2(size, size));
+    _brushPreview->set_position(Vector2(-(size / 2), -(size / 2)));
+
+    _valueLabel->set_text(String::num(value));
+}
+
+void BrushNumericSelector::requestSelectValue() {
+    auto distance = getMouseDistance();
+    if (!_onValueSelected.is_null()) {
+        _onValueSelected.call(distance);
+    }
+}
---
+++ src/editor_nodes/brush_numeric_selector.h
@@ -0,0 +1,54 @@
+#ifndef BRUSH_NUMERIC_SELECTOR_H
+#define BRUSH_NUMERIC_SELECTOR_H
+
+#include <godot_cpp/classes/control.hpp>
+#include <godot_cpp/classes/texture_rect.hpp>
+#include <godot_cpp/classes/label.hpp>
+#include <godot_cpp/classes/input_event.hpp>
+
+using namespace godot;
+
+class BrushNumericSelector : public Control {
+    GDCLASS(BrushNumericSelector, Control)
+
+private:
+    const int BackgroundMargin = 10;
+
+    TextureRect *_background;
+    TextureRect *_brushPreview;
+    Label *_valueLabel;
+
+    int _brushSizeFactor;
+    Color _widgetColor;
+    int _minValue;
+    int _maxValue;
+
+    Callable _onValueSelected;
+    Callable _onCancel;
+
+    int getMouseDistance();
+
+protected:
+    static void _bind_methods();
+
+public:
+    BrushNumericSelector();
+    ~BrushNumericSelector();
+
+    void _ready() override;
+    void _process(double delta) override;
+    void _gui_input(const Ref<InputEvent> &event) override;
+
+    void set_brushSizeFactor(const int value);
+    void set_widgetColor(const Color value);
+    void set_minValue(const int value);
+    void set_maxValue(const int value);
+
+    void set_onValueSelected(const Callable value);
+    void set_onCancel(const Callable value);
+
+    void setInitialValue(float value);
+    void updateValue(float value);
+    void requestSelectValue();
+};
+#endif
