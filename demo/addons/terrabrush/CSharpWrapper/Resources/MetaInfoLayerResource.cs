b9cfbd2a31a5fd87c1833fb1f38d204997bb8dda
spimort <spimort@gmail.com>
2025-10-13 16:50:30 -0400

    Handle MetaInfoLayerResource wrapper

--- demo/addons/terrabrush/CSharpWrapper/Resources/MetaInfoLayerResource.cs
+++ demo/addons/terrabrush/CSharpWrapper/Resources/MetaInfoLayerResource.cs
@@ -1,10 +1,41 @@
-// using Godot;
+using Godot;
 
-// namespace TerraBrush;
+namespace TerraBrush;
 
-// [Tool]
-// [GlobalClass]
-// public partial class MetaInfoLayer : Resource {
-//     [Export] public string Name { get;set; }
-//     [Export] public Color Color { get;set; } = Colors.Black;
-// }
+public partial class MetaInfoLayerResource : Resource {
+    private Variant _godotHandle;
+
+    public string Name {
+        get {
+            return _godotHandle.AsGodotObject().Call("get_name").AsString();
+        }
+        set {
+            _godotHandle.AsGodotObject().Call("set_name", value);
+        }
+    }
+
+    public Color Color {
+        get {
+            return _godotHandle.AsGodotObject().Call("get_color").AsColor();
+        }
+        set {
+            _godotHandle.AsGodotObject().Call("set_color", value);
+        }
+    }
+
+    public static implicit operator MetaInfoLayerResource(Variant handle) => new(handle);
+
+    public MetaInfoLayerResource(Variant handle) {
+        _godotHandle = handle;
+    }
+
+    public MetaInfoLayerResource() {
+        _godotHandle = ClassDB.Instantiate("MetaInfoLayerResource");
+    }
+
+    internal Variant GodotHandle() {
+        return _godotHandle;
+    }
+
+    public new string GetPath() => _godotHandle.AsGodotObject().Call("get_path").AsString();
+}
