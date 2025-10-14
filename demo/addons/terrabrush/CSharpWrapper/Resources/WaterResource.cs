c1b5fa77f4cd542dc6d3ab946776e310eb197f5e
spimort <spimort@gmail.com>
2025-10-13 22:52:55 -0400

    Handle WaterResource wrapper

--- demo/addons/terrabrush/CSharpWrapper/Resources/WaterResource.cs
+++ demo/addons/terrabrush/CSharpWrapper/Resources/WaterResource.cs
@@ -1,51 +1,231 @@
-// using System;
-// using Godot;
+using System;
+using Godot;
 
-// namespace TerraBrush;
+namespace TerraBrush;
 
-// [Tool]
-// [GlobalClass]
-// public partial class WaterResource : Resource {
-//     private ShaderMaterial _customShader;
+public partial class WaterResource : Resource {
+    private Variant _godotHandle;
 
-//     [Export] public float WaterFactor { get;set; } = 1.0f;
-//     [Export] public float WaterInnerOffset { get;set; } = 0.2f;
-//     [Export(PropertyHint.ColorNoAlpha)] public Color WaterColor { get;set; } = Colors.White;
-//     [Export(PropertyHint.ColorNoAlpha)] public Color WaterFresnelColor { get;set; } = Colors.White;
-//     [Export(PropertyHint.Range, "0,1,0.001")] public float WaterMetallic { get;set; }
-//     [Export(PropertyHint.Range, "0,1,0.001")] public float WaterRoughness { get;set; }
-//     [Export] public Texture2D WaterNormalMap { get;set; }
-//     [Export] public Texture2D WaterNormalMap2 { get;set; }
-//     [Export] public float WaterTimeScale { get;set; } = 0.1f;
-//     [Export] public float WaterStrength { get;set; } = 0.4f;
-//     [Export] public Texture2D WaterWave { get;set; }
-//     [Export] public float WaterNoiseScale { get;set; } = 10.0f;
-//     [Export] public float WaterHeightScale { get;set; } = 0.15f;
-//     [Export] public Color WaterColorDeep { get;set; } = Colors.White;
-//     [Export] public Color WaterColorShallow { get;set; } = Colors.White;
-//     [Export] public float WaterBeersLaw { get;set; } = 2.0f;
-//     [Export] public float WaterDepthOffset { get;set; } = -0.75f;
-//     [Export] public float WaterEdgeScale { get;set; } = 0.1f;
-//     [Export] public float WaterNear { get;set; } = 0.5f;
-//     [Export] public float WaterFar { get;set; } = 100f;
-//     [Export(PropertyHint.ColorNoAlpha)] public Color WaterEdgeColor { get;set; } = Colors.White;
-//     [Export(PropertyHint.Layers3DRender)] public int VisualInstanceLayers { get;set; } = 1;
+    public float WaterFactor {
+        get {
+            return (float) _godotHandle.AsGodotObject().Call("get_waterFactor").AsDouble();
+        }
+        set {
+            _godotHandle.AsGodotObject().Call("set_waterFactor", value);
+        }
+    }
 
-//     [Export] public ShaderMaterial CustomShader {
-//         get {
-//             return _customShader;
-//         } set {
-//             _customShader = value;
+    public float WaterInnerOffset {
+        get {
+            return (float) _godotHandle.AsGodotObject().Call("get_waterInnerOffset").AsDouble();
+        }
+        set {
+            _godotHandle.AsGodotObject().Call("set_waterInnerOffset", value);
+        }
+    }
 
-//             if (value != null && value.Shader == null) {
-//                 var defaultShader = ResourceLoader.Load<Shader>("res://addons/terrabrush/Resources/Shaders/water_clipmap_shader.gdshader");
-//                 var defaultCode = defaultShader.Code;
+    public Color WaterColor {
+        get {
+            return _godotHandle.AsGodotObject().Call("get_waterColor").AsColor();
+        }
+        set {
+            _godotHandle.AsGodotObject().Call("set_waterColor", value);
+        }
+    }
 
-//                 var shader = new Shader {
-//                     Code = defaultCode
-//                 };
-//                 value.Shader = shader;
-//             }
-//         }
-//     }
-// }
+    public Color WaterFresnelColor {
+        get {
+            return _godotHandle.AsGodotObject().Call("get_waterFresnelColor").AsColor();
+        }
+        set {
+            _godotHandle.AsGodotObject().Call("set_waterFresnelColor", value);
+        }
+    }
+
+    public float WaterMetallic {
+        get {
+            return (float) _godotHandle.AsGodotObject().Call("get_waterMetallic").AsDouble();
+        }
+        set {
+            _godotHandle.AsGodotObject().Call("set_waterMetallic", value);
+        }
+    }
+
+    public float WaterRoughness {
+        get {
+            return (float) _godotHandle.AsGodotObject().Call("get_waterRoughness").AsDouble();
+        }
+        set {
+            _godotHandle.AsGodotObject().Call("set_waterRoughness", value);
+        }
+    }
+
+    public Texture2D WaterNormalMap {
+        get {
+            return _godotHandle.AsGodotObject().Call("get_waterNormalMap").As<Texture2D>();
+        }
+        set {
+            _godotHandle.AsGodotObject().Call("set_waterNormalMap", value);
+        }
+    }
+
+    public Texture2D WaterNormalMap2 {
+        get {
+            return _godotHandle.AsGodotObject().Call("get_waterNormalMap2").As<Texture2D>();
+        }
+        set {
+            _godotHandle.AsGodotObject().Call("set_waterNormalMap2", value);
+        }
+    }
+
+    public float WaterTimeScale {
+        get {
+            return (float) _godotHandle.AsGodotObject().Call("get_waterTimeScale").AsDouble();
+        }
+        set {
+            _godotHandle.AsGodotObject().Call("set_waterTimeScale", value);
+        }
+    }
+
+    public float WaterStrength {
+        get {
+            return (float) _godotHandle.AsGodotObject().Call("get_waterStrength").AsDouble();
+        }
+        set {
+            _godotHandle.AsGodotObject().Call("set_waterStrength", value);
+        }
+    }
+
+    public Texture2D WaterWave {
+        get {
+            return _godotHandle.AsGodotObject().Call("get_waterWave").As<Texture2D>();
+        }
+        set {
+            _godotHandle.AsGodotObject().Call("set_waterWave", value);
+        }
+    }
+
+    public float WaterNoiseScale {
+        get {
+            return (float) _godotHandle.AsGodotObject().Call("get_waterNoiseScale").AsDouble();
+        }
+        set {
+            _godotHandle.AsGodotObject().Call("set_waterNoiseScale", value);
+        }
+    }
+
+    public float WaterHeightScale {
+        get {
+            return (float) _godotHandle.AsGodotObject().Call("get_waterHeightScale").AsDouble();
+        }
+        set {
+            _godotHandle.AsGodotObject().Call("set_waterHeightScale", value);
+        }
+    }
+
+    public Color WaterColorDeep {
+        get {
+            return _godotHandle.AsGodotObject().Call("get_waterColorDeep").AsColor();
+        }
+        set {
+            _godotHandle.AsGodotObject().Call("set_waterColorDeep", value);
+        }
+    }
+
+    public Color WaterColorShallow {
+        get {
+            return _godotHandle.AsGodotObject().Call("get_waterColorShallow").AsColor();
+        }
+        set {
+            _godotHandle.AsGodotObject().Call("set_waterColorShallow", value);
+        }
+    }
+
+    public float WaterBeersLaw {
+        get {
+            return (float) _godotHandle.AsGodotObject().Call("get_waterBeersLaw").AsDouble();
+        }
+        set {
+            _godotHandle.AsGodotObject().Call("set_waterBeersLaw", value);
+        }
+    }
+
+    public float WaterDepthOffset {
+        get {
+            return (float) _godotHandle.AsGodotObject().Call("get_waterDepthOffset").AsDouble();
+        }
+        set {
+            _godotHandle.AsGodotObject().Call("set_waterDepthOffset", value);
+        }
+    }
+
+    public float WaterEdgeScale {
+        get {
+            return (float) _godotHandle.AsGodotObject().Call("get_waterEdgeScale").AsDouble();
+        }
+        set {
+            _godotHandle.AsGodotObject().Call("set_waterEdgeScale", value);
+        }
+    }
+
+    public float WaterNear {
+        get {
+            return (float) _godotHandle.AsGodotObject().Call("get_waterNear").AsDouble();
+        }
+        set {
+            _godotHandle.AsGodotObject().Call("set_waterNear", value);
+        }
+    }
+
+    public float WaterFar {
+        get {
+            return (float) _godotHandle.AsGodotObject().Call("get_waterFar").AsDouble();
+        }
+        set {
+            _godotHandle.AsGodotObject().Call("set_waterFar", value);
+        }
+    }
+
+    public Color WaterEdgeColor {
+        get {
+            return _godotHandle.AsGodotObject().Call("get_waterEdgeColor").AsColor();
+        }
+        set {
+            _godotHandle.AsGodotObject().Call("set_waterEdgeColor", value);
+        }
+    }
+
+    public int VisualInstanceLayers {
+        get {
+            return _godotHandle.AsGodotObject().Call("get_visualInstanceLayers").AsInt32();
+        }
+        set {
+            _godotHandle.AsGodotObject().Call("set_visualInstanceLayers", value);
+        }
+    }
+
+    public ShaderMaterial CustomShader {
+        get {
+            return _godotHandle.AsGodotObject().Call("get_customShader").As<ShaderMaterial>();
+        }
+        set {
+            _godotHandle.AsGodotObject().Call("set_customShader", value);
+        }
+    }
+
+    public static implicit operator WaterResource(Variant handle) => new(handle);
+
+    public WaterResource(Variant handle) {
+        _godotHandle = handle;
+    }
+
+    public WaterResource() {
+        _godotHandle = ClassDB.Instantiate("WaterResource");
+    }
+
+    internal Variant GodotHandle() {
+        return _godotHandle;
+    }
+
+    public new string GetPath() => _godotHandle.AsGodotObject().Call("get_path").AsString();
+}
