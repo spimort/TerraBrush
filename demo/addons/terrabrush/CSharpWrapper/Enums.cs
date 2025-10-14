a68d16dbb49363ae9e13ee2b2eaf125572b5e2ac
spimort <spimort@gmail.com>
2025-10-13 23:30:21 -0400

    Handle TerraBrush wrapper

--- demo/addons/terrabrush/CSharpWrapper/Enums.cs
+++ demo/addons/terrabrush/CSharpWrapper/Enums.cs
@@ -9,3 +9,15 @@ public enum ObjectStrategy {
     PackedScenes = 1,
     OctreeMultiMeshes = 2
 }
+
+public enum AlphaChannelUsage {
+    None = 0,
+    Roughness = 1,
+    Height = 2
+}
+
+public enum ObjectLoadingStrategy {
+    ThreadedInEditorOnly = 1,
+    Threaded = 2,
+    NotThreaded = 3
+}
