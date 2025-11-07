8658d1ba99b8340714d84518ef42476e47f223bf
spimort <spimort@gmail.com>
2025-11-06 23:19:20 -0500

    Change default brush color

--- src/misc/setting_contants.cpp
+++ src/misc/setting_contants.cpp
@@ -7,7 +7,7 @@ const StringName SettingContants::CustomBrushesFolder() { return "terraBrush/bru
 const StringName SettingContants::SculptingMultiplier() { return "terraBrush/brushes/sculpting_multiplier"; }
 const StringName SettingContants::IconsColor() { return "terraBrush/interface/icons_color"; }
 
-const Color SettingContants::DecalColorDefaultValue() { return Color(1.0f, 0, 0, 0.5f); }
+const Color SettingContants::DecalColorDefaultValue() { return Color::html("#244ab580"); }
 const StringName SettingContants::CustomBrushesFolderDefaultValue() { return "res://TerraBrush_CustomBrushes"; }
 const int SettingContants::SculptingMultiplierDefaultValue() { return 10; }
 const Color SettingContants::IconsColorDefaultValue() { return Color::html("#00151F"); }
