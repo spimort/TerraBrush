d21461b99de16d7b7771b94a542f8a6e07b5a2bb
spimort <spimort@gmail.com>
2025-12-15 20:39:12 -0500

    Add a setting to show/hide the brush bubble

--- src/misc/setting_contants.cpp
+++ src/misc/setting_contants.cpp
@@ -2,11 +2,13 @@
 
 using namespace godot;
 
+const StringName SettingContants::ShowDecalBubble() { return "terraBrush/brushes/show_decal_bubble"; }
 const StringName SettingContants::DecalColor() { return "terraBrush/brushes/decal_color"; }
 const StringName SettingContants::CustomBrushesFolder() { return "terraBrush/brushes/custom_brushes_folder"; }
 const StringName SettingContants::SculptingMultiplier() { return "terraBrush/brushes/sculpting_multiplier"; }
 const StringName SettingContants::IconsColor() { return "terraBrush/interface/icons_color"; }
 
+const bool SettingContants::ShowDecalBubbleDefaultValue() { return true; }
 const Color SettingContants::DecalColorDefaultValue() { return Color::html("#244ab580"); }
 const StringName SettingContants::CustomBrushesFolderDefaultValue() { return "res://TerraBrush_CustomBrushes"; }
 const int SettingContants::SculptingMultiplierDefaultValue() { return 10; }
