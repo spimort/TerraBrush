b7f79347f70e32e091f649f207663a492c44b42f
spimort <spimort@gmail.com>
2026-04-03 12:54:38 -0400

    Add debug script

--- debug-tools/run-godot-4.6.1.sh
+++
@@ -1,1 +0,0 @@
-./../../Godot_v4.6.1-stable_linux.x86_64/Godot_v4.6.1-stable_linux.x86_64 -e --path "demo"
---
+++ debug.sh
@@ -0,0 +1,3 @@
+# This assumes that you have gdvm (https://github.com/adalinesimonian/gdvm) installed
+# gdvm is an amazing godot version manager give it a star 🙂
+godot_console -e --path "demo"
