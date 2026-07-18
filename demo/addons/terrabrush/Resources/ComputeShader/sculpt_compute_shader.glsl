510813d654f164926ee362172aedcc4ccdc25d44
spimort <spimort@gmail.com>
2026-07-17 21:45:08 -0400

    Update shaders

--- demo/addons/terrabrush/Resources/ComputeShader/sculpt_compute_shader.glsl
+++ demo/addons/terrabrush/Resources/ComputeShader/sculpt_compute_shader.glsl
@@ -3,10 +3,9 @@
 
 layout(local_size_x = 8, local_size_y = 8, local_size_z = 1) in;
 
-layout(r8, binding = 0) restrict uniform image2DArray heightmapTextures;
+layout(r8, binding = 0) restrict uniform image2D heightmapTexture;
 layout(rgba8, binding = 1) restrict readonly uniform image2D brushTexture; ;
 layout(set = 0, binding = 2) uniform Params {
-    int numberOfHeightmaps;
     float brushStrength;
     int add;
 } params;
@@ -14,18 +13,14 @@ layout(set = 0, binding = 2) uniform Params {
 void main() {
     ivec2 coords = ivec2(gl_GlobalInvocationID.xy);
     vec4 brushPixel = imageLoad(brushTexture, coords);
+    vec4 heightmapPixel = imageLoad(heightmapTexture, coords);
 
-    for (int i = 0; i < params.numberOfHeightmaps; i++) {
-        ivec3 heightmapCoords = ivec3(coords, i);
-        vec4 heightmapPixel = imageLoad(heightmapTextures, heightmapCoords);
-
-        float sculptValue = brushPixel.a * params.brushStrength;
-        if (params.add != 1) {
-            sculptValue *= -1.0;
-        }
-
-        heightmapPixel.r += sculptValue;
-
-        imageStore(heightmapTextures, heightmapCoords, heightmapPixel);
+    float sculptValue = brushPixel.a * params.brushStrength;
+    if (params.add != 1) {
+        sculptValue *= -1.0;
     }
+
+    heightmapPixel.r += sculptValue;
+
+    imageStore(heightmapTexture, coords, heightmapPixel);
 }
--- demo/addons/terrabrush/Resources/ComputeShader/smooth_compute_shader.glsl
+++ demo/addons/terrabrush/Resources/ComputeShader/smooth_compute_shader.glsl
@@ -3,10 +3,9 @@
 
 layout(local_size_x = 8, local_size_y = 8, local_size_z = 1) in;
 
-layout(r8, binding = 0) restrict uniform image2DArray heightmapTextures;
+layout(r8, binding = 0) restrict uniform image2D heightmapTexture;
 layout(rgba8, binding = 1) restrict readonly uniform image2D brushTexture; ;
 layout(set = 0, binding = 2) uniform Params {
-    int numberOfHeightmaps;
     float brushStrength;
 } params;
 
@@ -14,20 +13,16 @@ void main() {
     ivec2 coords = ivec2(gl_GlobalInvocationID.xy);
     vec4 brushPixel = imageLoad(brushTexture, coords);
 
-    for (int i = 0; i < params.numberOfHeightmaps; i++) {
-        ivec3 heightmapCoords = ivec3(coords, i);
+    vec4 leftHeightmapPixel = imageLoad(heightmapTexture, coords - ivec2(1, 0));
+    vec4 topHeightmapPixel = imageLoad(heightmapTexture, coords - ivec2(0, 1));
+    vec4 rightHeightmapPixel = imageLoad(heightmapTexture, coords + ivec2(1, 0));
+    vec4 bottomHeightmapPixel = imageLoad(heightmapTexture, coords + ivec2(0, 1));
 
-        vec4 leftHeightmapPixel = imageLoad(heightmapTextures, heightmapCoords - ivec3(1, 0, i));
-        vec4 topHeightmapPixel = imageLoad(heightmapTextures, heightmapCoords - ivec3(0, 1, i));
-        vec4 rightHeightmapPixel = imageLoad(heightmapTextures, heightmapCoords + ivec3(1, 0, i));
-        vec4 bottomHeightmapPixel = imageLoad(heightmapTextures, heightmapCoords + ivec3(0, 1, i));
+    vec4 heightmapPixel = imageLoad(heightmapTexture, coords);
 
-        vec4 heightmapPixel = imageLoad(heightmapTextures, heightmapCoords);
+    float average = (leftHeightmapPixel.r + topHeightmapPixel.r + rightHeightmapPixel.r + bottomHeightmapPixel.r + heightmapPixel.r) / 5.0;
 
-        float average = (leftHeightmapPixel.r + topHeightmapPixel.r + rightHeightmapPixel.r + bottomHeightmapPixel.r + heightmapPixel.r) / 5.0;
+    heightmapPixel.r = mix(heightmapPixel.r, average, brushPixel.a * params.brushStrength);
 
-        heightmapPixel.r = mix(heightmapPixel.r, average, brushPixel.a * params.brushStrength);
-
-        imageStore(heightmapTextures, heightmapCoords, heightmapPixel);
-    }
+    imageStore(heightmapTexture, coords, heightmapPixel);
 }
